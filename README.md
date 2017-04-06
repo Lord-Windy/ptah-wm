ccd1d3de02e7970a5e785ef5375aceb34cf33f30
Samuel Brown <sam@samuelbrown.me>
2017-04-06 11:07:58 +1000

    Re-uploading previous information. Will make changes after this

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# ptah-wm
+# React WM [![Build Status](https://travis-ci.org/RoscoeH/react-wm.svg?branch=master)](https://travis-ci.org/RoscoeH/react-wm)
+## Getting Started
+1. Install Node
+2. Install Webpack and the Webpack-Dev-Server
+
+    ```
+    npm install -g webpack webpack-dev-server
+    ```
+3. Clone the repo
+4. Install the dependencies and start the server.
+
+    ```
+    cd react-wm
+    npm install
+    npm start
+    ```
+5. Open http://localhost:3000 in your browser
+
+## Component Tree
+![Component Tree](https://www.lucidchart.com/publicSegments/view/a1b41edd-36b8-457d-b7b8-923e64675612/image.png)
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://0.0.0.0:80',
+    'webpack/hot/only-dev-server',
+    './lib/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['react-hot', 'babel'],
+        include: path.join(__dirname, 'lib')
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style!css!sass'
+      },
+      {
+	 test: /\.json$/,
+        loader: 'json'
+      },
+
+    ]
+  },
+  resolve: {
+    root: path.resolve('./lib'),
+    extensions: ['', '.js']
+  }
+};
